4af560b6f1eb98c03e084a022ef1367852b7e214
xqft <estefano.bargas@fing.edu.uy>
2024-12-29 17:44:30 -0300

    signal received message

--- src/net/udp.rs
+++ src/net/udp.rs
@@ -1,44 +1,45 @@
-use core::str::from_utf8;
-
 use embassy_executor::Spawner;
 use embassy_net::udp::{PacketMetadata, UdpSocket};
 use embassy_net::Stack;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::signal::Signal;
 use esp_wifi::wifi::{WifiDevice, WifiStaDevice};
-use log::{info, warn};
+use log::{debug, warn};
 
 const PORT: u16 = 8080;
+const MSG_MAX_LEN: usize = 32;
+
+pub static RX_MSG: Signal<CriticalSectionRawMutex, (usize, [u8; MSG_MAX_LEN])> = Signal::new();
 
 #[embassy_executor::task]
-async fn handle(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
+async fn rx(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
     let mut rx_meta = [PacketMetadata::EMPTY; 16];
     let mut rx_buffer = [0; 1024];
 
-    // empty as there will be no transmission
-    let mut tx_meta = [PacketMetadata::EMPTY; 0];
-    let mut tx_buffer = [0; 0];
-
-    let mut msg_buffer = [0; 32];
-
-    let mut udp_socket = UdpSocket::new(
-        stack,
-        &mut rx_meta,
-        &mut rx_buffer,
-        &mut tx_meta,
-        &mut tx_buffer,
-    );
+    // empty tx buffers because this is rx only
+    let mut udp_socket = UdpSocket::new(stack, &mut rx_meta, &mut rx_buffer, &mut [], &mut []);
     udp_socket.bind(PORT).unwrap();
 
+    let mut msg_buffer = [0; MSG_MAX_LEN];
     loop {
-        let (_, from_addr) = udp_socket.recv_from(&mut msg_buffer).await.unwrap();
-        let Ok(msg) = from_utf8(&msg_buffer) else {
-            warn!("received invalid utf8 message from {from_addr}");
+        let Ok((msg_len, from_addr)) = udp_socket.recv_from(&mut msg_buffer).await else {
+            warn!(
+                "received message longer than msg buffer (which has length: {})",
+                msg_buffer.len()
+            );
+            debug!("truncated message: {msg_buffer:?}");
             continue;
         };
 
-        info!("received message {msg} from {from_addr}");
+        RX_MSG.signal((msg_len, msg_buffer));
+
+        debug!(
+            "received message {:?} from {from_addr}",
+            &msg_buffer[..msg_len]
+        );
     }
 }
 
 pub fn register(spawner: &Spawner, stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
-    spawner.spawn(handle(stack)).unwrap();
+    spawner.spawn(rx(stack)).unwrap();
 }
